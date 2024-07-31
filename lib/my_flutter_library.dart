1fd8fb340d835c4ddb8f52d8ab687506c8d86af0
Akshay898989 <akshay.gupta8989@gmail.com>
2024-07-31 17:22:20 +0530

    Initial commit

---
+++ lib/my_flutter_library.dart
@@ -0,0 +1,18 @@
+library my_flutter_library;
+
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  MyButton({required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(text),
+    );
+  }
+}
